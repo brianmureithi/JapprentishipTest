76f86f5fd8246e92fe87407b5831ccfedf900eff
brian mureithi <brianmurithi65@gmail.com>
2020-12-15 10:12:25 +0300

    My first Commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react';
+import {AppBar,Badge,
+  CssBaseline,
+  Container, CircularProgress,Typography,TextField,Tab,Tabs,Chip,FormControlLabel,Checkbox, Button} from '@material-ui/core';
+import {makeStyles,withStyles} from '@material-ui/core/styles'
+import JCard from './Card';
+
+
+const myStyles=makeStyles({
+  form: {
+    display:'flex',
+    alignItems:'center',
+    justifyContent: 'center',
+    '& > *':{
+      margin:20,
+      width:'25ch',
+    },
+  },
+
+});
+function Spinner(){
+  return (
+    <div style={{textAlign:'center',padding:'2rem'}}>
+      <CircularProgress/>
+    </div>
+  )
+}
 
 function App() {
+  const [jokes,setJokes] =useState([]);
+  const [jokeToShow, setJokesToShow] = useState([]);
+  const [categories,setCategories] = useState([]);
+  const [filterCategories,setFilterCategories] = useState([]);
+
+
+ 
+  const [likedJokes,setLikedJokes]= useState([]);
+  const [currentTab,setCurrentTab] = useState(0);
+  const [firstName,setFirstName] = useState ('Chuck');
+  const [lastName,setLastName] = useState ('Norris');
+
+
+  const [loading,setLoading] =useState(false);
+  const classes = myStyles();
+  
+
+
+  useEffect( () => {
+    setLoading(true)
+    fetchAndSetJokes()
+    fetch('https://api.icndb.com/categories').
+    then( res => res.json()).then(res =>{
+      setCategories(res.value);
+      setFilterCategories(res.value)
+      setLoading(false)
+    })
+    .catch(err   => console.log(err))
+  },[]);
+const fetchAndSetJokes = ()=>{
+  fetch( `https://api.icndb.com/jokes?firstName=${firstName}&lastName=${lastName}`).then(
+    (res) => res.json()
+   ).then((res) =>{
+     console.log(res);
+     setJokes(res.value);
+     setJokesToShow(res.value.slice(0,10));
+     setLoading(false)
+  })
+   .catch((err) => console.log(err));
+}
+
+      const likeJoke = (id) => {
+      if(likedJokes.find(j => j.id ===id)) return
+      const likedJoke = jokes.find(j => j.id ===id);
+      setLikedJokes([likedJoke,...likedJokes])
+      } 
+
+     
+      const unlikeJoke = (id) => {
+     const newLikedJokes = likedJokes.filter(j => j.id !==id)
+     setLikedJokes(newLikedJokes);
+      }
+      const changeTab = (event, value) =>{
+        setCurrentTab(value)
+      }
+      const addMoreJokes =() => {
+         setLoading(true)
+         setTimeout(()=> {
+          setJokesToShow(jokes.slice(0,jokeToShow.length + 10));
+          setLoading(false)
+
+         },400);
+        
+      }
+
+        const observeElement = (bottomJoke) =>{   
+          if(!bottomJoke) return         
+const observer = new IntersectionObserver((entries)=>{
+if(entries[0].isIntersecting ===  true){
+  console.log('Adding more jokes')
+ addMoreJokes();
+ observer.unobserve(bottomJoke)
+}
+
+},
+{
+threshold: 1
+}  
+);     
+observer.observe(bottomJoke);
+};
+
+useEffect(()=>{
+  const bottomJokeE1 = document.
+  getElementById(`joke-${jokeToShow.length - 1}`);
+
+  observeElement(bottomJokeE1);
+},[jokeToShow]) ;  
+
+const toggleCategory = (event)=> {
+  const category = event.target.name
+  if(filterCategories.includes(category)){
+    const filterCategoriesCopy = [...filterCategories]
+ const categoryIndex = filterCategoriesCopy.indexOf(category)
+  filterCategoriesCopy.splice(categoryIndex, 1)
+  setFilterCategories(filterCategoriesCopy)
+  }
+  else{
+    setFilterCategories([...filterCategories,category])
+  }
+}
+
+const categoryMatch = (jokeCategories)=> {
+  for(let i=0; i < jokeCategories.length; i++){
+    if(filterCategories.includes(jokeCategories[i]))
+    return true; 
+  }
+return false;
+}
+
+const changeJokes = (e) =>{
+  e.preventDefault();
+  if(firstName ==='' || lastName ==='') return
+  fetchAndSetJokes()
+
+}
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <CssBaseline/>
+        <Container>
+        <Typography variant="h3" align="center" style={{marginTop:'50px'}}>
+        Apprenticeship Program Test
+      </Typography>
+      <AppBar style={{marginBottom:10}}>
+      <Tabs value={ currentTab} onChange={changeTab}>
+        <Tab label="Home" id="home-tab" aria-controls="home-panel"/>
+        <Tab label={
+          <Badge
+          color="secondary"
+          badgeContent={
+            likedJokes.length > 0 ? likedJokes.length: null
+          }>
+            Likes
+          </Badge>
+        } id="like-tab" aria-controls="like-panel"/>
+      </Tabs>
+      </AppBar>
+      <div role=" tabpanel" hidden ={currentTab !==0}>
+       <div style={{display: 'flex',flexDirection: 'column',justifyContent:'center'}}>
+       <form onSubmit ={changeJokes} noValidate className={classes.form} >
+          <TextField id ="firstName"  label = "First value" value={firstName}
+          onChange={ e => setFirstName (e.target.value)}/>
+           <TextField id ="lastName"  label = "Last value" value={lastName  }
+          onChange={ e => setLastName (e.target.value)}/>
+<Button type="submit" variant="contained" color="primary">submit</Button>
+        </form>
+
+       </div>
+        {categories.map(category=>(
+          <FormControlLabel key={category} control={<Checkbox
+          name={category} color="primary" 
+          checked={filterCategories.includes(category) } onChange ={toggleCategory}/>}
+          label={category}/>
+
+        
+        ))}
+      {jokeToShow.map((joke,index) =>{
+        if (joke.categories.length === 0 || categoryMatch(joke.categories)){
+        return(
+        <JCard key={joke.id } 
+        joke={joke} 
+        likeJoke={likeJoke}
+         unlikeJoke={unlikeJoke} index={index}/>
+      );
+        }
+      })}
+      {loading && <Spinner/>}
+      </div>
+     <div role="tabpanel" hidden ={currentTab !==1}>
+     {likedJokes.map(joke =>(
+      <JCard key={joke.id }joke={joke} likeJoke={likeJoke} unlikeJoke={unlikeJoke}/>
+      ))}
+     </div>
+</Container>
+    
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Card,
+    CardContent,
+    CardActions,
+    Chip,
+    Typography,
+    Button} 
+from '@material-ui/core'
+import {makeStyles,withStyles} from '@material-ui/core/styles'
+
+const myStyles = makeStyles({
+    card : {
+      marginBottom:20
+    },
+    CardContent:{
+      paddingBottom:5
+    },
+    CardActions:{
+      padding: 16
+    }
+  });
+  
+  const Category= withStyles({
+    root :{
+      marginTop:10,
+     marginBottom:10
+  
+    }
+  })(Chip)
+  
+export default function JCard({joke,likeJoke,unlikeJoke,index }){
+    const classes = myStyles();
+    return(
+
+<Card key={joke.id} className={classes.card} id={`joke-${index}`}>
+<CardContent  className={classes.CardContent}>
+{joke.categories.length > 0 ? (
+  joke.categories.map((cat) => (
+    <Category label={cat} key={cat} variant='outlined'style={{background:'#1e7984',color:'#fff'}}/>
+    
+  ))
+): (<Category label = 'Uncategorized' variant = 'outlined' style={{background:'#7e7c71'}}/> )}
+  <Typography>
+    {joke.joke}
+  </Typography>
+</CardContent>
+<CardActions className={classes.cardActions}>
+  <Button variant="contained" color="primary" onClick= {() => likeJoke(joke.id)}>Like</Button>
+  <Button variant="contained" color="default"  onClick= {() => unlikeJoke(joke.id)}>Unlike</Button>
+</CardActions>
+</Card>
+    )
+}
